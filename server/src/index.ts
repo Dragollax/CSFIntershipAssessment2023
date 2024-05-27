60c7bd97285995e41803643f01573c480d37ed94
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2024-05-27 01:26:07 -0400

    fix bug with wrong function getting called for setting status

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,31 +29,37 @@ export const router: Router = express.Router();
 router.use(express.json());
 
 router.post('/', async (req, res) => {
-  console.log(req.body);
   let {name, uselessFact, rating} = req.body;
 
   if(name == undefined || name === null || 
     (typeof name !== 'string' && !(name instanceof String))  || 
     name.length  <= 0) {
-      return res.send(400).json({error: "Please enter a name."});
+      return res.status(400).json({error: "Please enter a name."});
     }
-  if(name.length >= charLimit) return res.send(400).json({error: `Please enter a name that is less than ${charLimit} characters.`});
-
+  if(name.length >= charLimit) {
+    return res.status(400).json({error: `Please enter a name that is less than ${charLimit} characters.`});
+  }
   if(uselessFact == undefined || uselessFact === null || 
     (typeof uselessFact !== 'string' && !(uselessFact instanceof String))  || 
     uselessFact.length  <= 0) {
-      return res.send(400).json({error: "Please enter a useless fact."});
+      return res.status(400).json({error: "Please enter a useless fact."});
     }
-  if(name.length >= charLimit) return res.send(400).json({error: `please enter a useless fact that is less than ${charLimit} characters.`});
+  if(name.length >= charLimit) {
+    return res.status(400).json({error: `please enter a useless fact that is less than ${charLimit} characters.`});
+  }
+  if(typeof rating !== 'number' || rating < 0 || rating > 1) {
+    return res.status(400).json({error: 'please enter a rating that is number and is greater than or equal to 0 and less than or equal to 1.'})
+  }
   
-  if(typeof rating !== 'number' || rating < 0 || rating > 1) return res.send(400).json({error: 'please enter a rating that is number and is greater than or equal to 0 and less than or equal to 1.'})
-  
-  
-  // let dbResp = await new uselessFacts({name: name, uselessFact: uselessFact, postedDate: Date.now(), rating: rating}).save();
+  try {
+    uselessFacts.create({name: name, uselessFact: uselessFact, postedDate: Date.now(), rating: rating});
+  } catch(e) {
+    console.log(e);
+    return res.status(500).send();
+  }
 
-  return res.sendStatus(200);
+  return res.status(200).send();
 });
-
 
 
 app.use('/', router);
