df8e459f163c2104f91801444703714e6d5335ac
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2024-05-26 20:28:47 -0400

    setup basic server

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express, {Router, Express} from 'express';
+import cors from 'cors';
+
+const port: number = 4000;
+
+const app: Express = express();
+app.use(cors());
+app.use(express.json());
+
+export const router: Router = express.Router();
+
+
+router.get('/', async (req, res) => {
+  return res.status(200).json({message: "test"});
+});
+
+
+
+
+app.use('/', router);
+app.listen(port, () => console.log("Server Started on " + port));
