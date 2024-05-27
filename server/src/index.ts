a314ff794f2277ff71c5b5052ab4149eb7a27a41
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2024-05-26 22:07:00 -0400

    removed mongo express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,20 @@
 import express, {Router, Express} from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const port: number = 4000;
 
+//setup express
 const app: Express = express();
 app.use(cors());
 app.use(express.json());
+
+
+//setup mongoose
+mongoose.connect("mongodb://root:password@localhost/CSF_Takehome")
+const db: mongoose.Connection = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
 export const router: Router = express.Router();
 
@@ -17,7 +26,6 @@ router.get('/', async (req, res) => {
 router.get('/something', async (req, res) => {
   return res.status(200).json({message: "test somethign else."});
 });
-
 
 app.use('/', router);
 app.listen(port, () => console.log("Server Started on " + port));
