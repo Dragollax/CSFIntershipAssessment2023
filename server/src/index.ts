c58575792454d444f1e8b428e3c453d17107044c
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2024-05-27 00:20:26 -0400

    add filtering for post route

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 const port: number = 4000;
+const charLimit: number = 10000;
 
 //setup express
 const app: Express = express();
@@ -11,21 +12,49 @@ app.use(express.json());
 
 
 //setup mongoose
-mongoose.connect("mongodb://root:password@localhost/CSF_Takehome")
+mongoose.connect("mongodb://database:27017/CSF_Takehome");
 const db: mongoose.Connection = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
+const uselessFactSchema = new mongoose.Schema({
+  name: String,
+  uselessFact: String,
+  postedDate: Date,
+  rating: Number
+});
+const uselessFacts = mongoose.model("uselessFacts", uselessFactSchema);
+
 
 export const router: Router = express.Router();
+router.use(express.json());
 
+router.post('/', async (req, res) => {
+  console.log(req.body);
+  let {name, uselessFact, rating} = req.body;
 
-router.get('/', async (req, res) => {
-  return res.status(200).json({message: "test"});
+  if(name == undefined || name === null || 
+    (typeof name !== 'string' && !(name instanceof String))  || 
+    name.length  <= 0) {
+      return res.send(400).json({error: "Please enter a name."});
+    }
+  if(name.length >= charLimit) return res.send(400).json({error: `Please enter a name that is less than ${charLimit} characters.`});
+
+  if(uselessFact == undefined || uselessFact === null || 
+    (typeof uselessFact !== 'string' && !(uselessFact instanceof String))  || 
+    uselessFact.length  <= 0) {
+      return res.send(400).json({error: "Please enter a useless fact."});
+    }
+  if(name.length >= charLimit) return res.send(400).json({error: `please enter a useless fact that is less than ${charLimit} characters.`});
+  
+  if(typeof rating !== 'number' || rating < 0 || rating > 1) return res.send(400).json({error: 'please enter a rating that is number and is greater than or equal to 0 and less than or equal to 1.'})
+  
+  
+  // let dbResp = await new uselessFacts({name: name, uselessFact: uselessFact, postedDate: Date.now(), rating: rating}).save();
+
+  return res.sendStatus(200);
 });
 
-router.get('/something', async (req, res) => {
-  return res.status(200).json({message: "test somethign else."});
-});
+
 
 app.use('/', router);
 app.listen(port, () => console.log("Server Started on " + port));
