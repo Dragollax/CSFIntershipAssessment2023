c0a5466a294a9ffa6635b60b88549710fa0b21a6
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2024-05-27 00:19:59 -0400

    add basic form and public api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Button, TextField } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todaysFact, setTodaysFact] = useState<string>("");
+
+  useEffect(() => {
+    (async () => {
+      let resp = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/today");
+      resp = await resp.json();
+      //@ts-ignore
+      setTodaysFact(resp.text);
+    })();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <p>Todays useless fact: {todaysFact}</p>
+    <TextField
+          id="outlined-textarea"
+          label="add a useless fact here"
+          placeholder="Placeholder"
+          multiline
+          style={{width: "100%"}}
+        />
+    <Button variant="contained">Submit</Button>
     </>
   )
 }
